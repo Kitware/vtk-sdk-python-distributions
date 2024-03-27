96e05ca2e52d923770289a1ec98d4284468d4874
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-03-27 11:27:04 -0400

    fix: Consolidate logic for setting download URL
    Use python, abi and platform tag terminology to match specification.
    See https://packaging.python.org/en/latest/specifications/platform-compatibility-tags/

--- cmake/vtk-sdk-urls.cmake
+++ cmake/vtk-sdk-urls.cmake
@@ -1,13 +1,35 @@
-# VTK SDK is currently stored as a tar.xz archive on a Kitware hosted server
-set(python_cp "cp${Python_VERSION_MAJOR}${Python_VERSION_MINOR}")
+# ----------------------------------------------------------------------------
+# Set URLs for downloading the VTK SDK stored as a tar.xz archive on Kitware
+# hosted server
 
-# enable any linux to download the right tar.xz
-if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-  set(VTK_SDK_BINARY_URL
-      "https://vtk.org/files/wheel-sdks/vtk-wheel-sdk-${VTK_VERSION}-${python_cp}-${python_cp}-manylinux_2_17_x86_64.manylinux2014_x86_64.tar.xz"
-  )
-else()
-  set(VTK_SDK_BINARY_URL
-      "https://vtk.org/files/wheel-sdks/vtk-wheel-sdk-${VTK_VERSION}-${python_cp}-${Python_SOABI}.tar.xz"
-  )
+message(STATUS "Setting VTK_SDK_BINARY_URL")
+
+# Python and ABI tags
+set(python_tag "cp${Python_VERSION_MAJOR}${Python_VERSION_MINOR}")
+set(abi_tag "${python_tag}")
+message(STATUS "  python_tag: ${python_tag}")
+message(STATUS "  abi_tag: ${abi_tag}")
+
+# Platform tag
+if(LINUX AND Python_SOABI MATCHES "x86_64")
+  set(plaform_tag "manylinux_2_17_x86_64.manylinux2014_x86_64")
+elseif(APPLE)
+  if(Python_SOABI MATCHES "x86_64")
+    set(plaform_tag "macosx_10_10_x86_64")
+  elseif(Python_SOABI MATCHES "arm64")
+    set(plaform_tag "macosx_11_0_arm64")
+  endif()
+elseif(WIN32)
+  set(plaform_tag "win_amd64")
 endif()
+if(NOT DEFINED plaform_tag)
+  message(
+    FATAL_ERROR
+      "Failed to set plaform_tag based of Python_SOABI [${Python_SOABI}]")
+endif()
+message(STATUS "  plaform_tag: ${plaform_tag}")
+
+set(VTK_SDK_BINARY_URL
+    "https://vtk.org/files/wheel-sdks/vtk-wheel-sdk-${VTK_VERSION}-${python_tag}-${abi_tag}-${plaform_tag}.tar.xz"
+)
+message(STATUS "Setting VTK_SDK_BINARY_URL: ${VTK_SDK_BINARY_URL}")
